6fde57caa8f0e51bd803f9ac0591586c55514af3
NNhatTThien <thientryhard@gmail.com>
2023-05-21 23:39:22 +0700

    Test rule

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React neeee
+          Learn React ne232323
         </a>
       </header>
     </div>
