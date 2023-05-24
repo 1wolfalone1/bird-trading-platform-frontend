abad437192d39132b2dcdebd41840c883b0124a2
NNhatTThien <thientryhard@gmail.com>
2023-05-24 22:54:57 +0700

    Finish login page, and add theme for material ui

--- src/container/common/layout/Layout.jsx
+++ src/container/common/layout/Layout.jsx
@@ -5,9 +5,47 @@ import Footer from "../footer/Footer";
 import s from './layout.module.scss'
 import { Outlet } from "react-router-dom";
 import clsx from "clsx";
+import { createTheme } from "@mui/material";
+import Style from "../../../style/inline-style/style";
+import { ThemeProvider } from "@emotion/react";
+import { red } from "@mui/material/colors";
+const theme = createTheme({
+   palette: {
+      Accent7: {
+         main: Style.color.$Accent7
+      },
+      Complementary: {
+         main: "#5e5e5e"
+      },
+      Dominant0: {
+         main:  "rgb(32, 72, 1)",
+      },
+      Dominant1: {
+         main:  "rgb(205, 255, 166)",
+
+      },
+      Dominant2: {
+         main: "rgb(228, 223, 209)",
+
+      },
+      Dominant3: {
+         main:  "rgb(255, 235, 235)",
+
+      },
+      Dominant4: {
+         main: "rgb(255, 255, 255)",
+
+      },
+      Dominant5: {
+         main: "rgb(205, 255, 166)",
+
+      }
+   },
+ });
 export default function Layout() {
    return (
       <>
+      <ThemeProvider theme={theme}>
          <Header/>
          <div className={clsx(s.content)}>
             <div className={clsx(s.extraSpace)}>
@@ -15,6 +53,7 @@ export default function Layout() {
             <Outlet/>
          </div>
          <Footer/>
+      </ThemeProvider>
       </>
    );
 }
