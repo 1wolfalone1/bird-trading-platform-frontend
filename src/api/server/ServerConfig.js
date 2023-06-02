df843bb349edc599be2f0fa8b6e03514bc3d2001
NNhatTThien <thientryhard@gmail.com>
2023-06-02 21:36:40 +0700

    Fix error can't access http server

--- src/api/server/ServerConfig.js
+++ src/api/server/ServerConfig.js
@@ -1,4 +1,4 @@
 
 
 export const MOCK_URL = "https://bird-trading-platform.free.mockoapp.net";
-export const BASE_URL = "http://ec2-13-250-162-106.ap-southeast-1.compute.amazonaws.com/api/v1";
+export const BASE_URL = "https://thongtienthienphuot.shop/api/v1";
