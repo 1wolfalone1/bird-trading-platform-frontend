af812c03d64434479069ec61805b4ba1e3b28f95
NNhatTThien <thientryhard@gmail.com>
2023-06-10 07:14:09 +0700

    Add redirect url to server

--- src/component/buttonGoogle/ButtonGoogle.jsx
+++ src/component/buttonGoogle/ButtonGoogle.jsx
@@ -3,12 +3,12 @@ import googleLogo from "../../asset/Google svg.svg";
 import clsx from "clsx";
 import s from "./ButtonGoole.module.scss";
 import RippleButton from "../button/ripple-button/RippleButton";
-import { URI_LOCAL_FRONTEND } from "../../config/constant";
+import { URI_FRONTEND } from "../../config/constant";
 
 export default function ButtonGoogle({ content, onClick }) {
    return (
       <a
-         href={`https://thongtienthienphuot.shop/oauth2/authorize/google?redirect_uri=${URI_LOCAL_FRONTEND}/get-token`}
+         href={`https://thongtienthienphuot.shop/oauth2/authorize/google?redirect_uri=${URI_FRONTEND}/get-token`}
          alt="dsfafasdfasdf"
          style={{display: 'flex', justifyContent: 'center'}}
       >
