6341cb5d5f8dc388764747536cc67327b5fcf3a8
Huỳnh Văn Phướt <phuothuynh2002@gmail.com>
2023-07-06 11:00:03 +0700

    rate products feature

--- src/component/purchase/order-status/header/action/Action.jsx
+++ src/component/purchase/order-status/header/action/Action.jsx
@@ -1,21 +1,9 @@
+import { Button, Modal } from "@mui/material";
 import clsx from "clsx";
 import React from "react";
-import s from "./action.module.scss";
-import { Box, Button, Modal, Typography } from "@mui/material";
 import ButtonChatNow from "../../../../message/button-chatnow/ButtonChatNow";
 import Rate from "../../rate/Rate";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+import s from "./action.module.scss";
 
 const cssButton = {
   border: "1px solid #000000",
@@ -28,12 +16,12 @@ const cssButton = {
 };
 
 export default function Action({ shopOwner, status, order }) {
-  console.log(order);
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const handleRate = () => {
-    if (status === "DELIVERIED") return true;
+    if (status == "DELIVERED") return false;
+    // return true;
   };
 
   return (
@@ -51,6 +39,9 @@ export default function Action({ shopOwner, status, order }) {
           onClose={handleClose}
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
+          BackdropProps={{
+            style: { pointerEvents: "none" },
+          }}
         >
           <Rate order={order} close={handleClose} />
         </Modal>
