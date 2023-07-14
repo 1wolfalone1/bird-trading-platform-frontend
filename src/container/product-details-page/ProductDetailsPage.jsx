b9674e09cf6a6da9563b1ca71217165b16f8ab26
Nguyen Van Thong <vanthong07012002@gmail.com>
2023-07-14 15:41:25 +0700

    add not found product in product detail

--- src/container/product-details-page/ProductDetailsPage.jsx
+++ src/container/product-details-page/ProductDetailsPage.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductDetails from "../product-details/ProductDetails";
 import ProductSuggest from "../product-details/product-suggest/ProductSuggest";
 import Rate from "../product-details/rate/Rate";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useRef } from "react";
+import { Box } from "@mui/material";
 
 export default function ProductDetailsPage() {
   const param = useParams();
   const containerRef = useRef(null);
+  const [isFound, setIsFound] = useState(true);
   useEffect(() => {
     handleScrollToTop();
   },[param])
@@ -18,12 +20,23 @@ export default function ProductDetailsPage() {
   };
 
   return (
-    <>
+    <>{isFound ? 
       <div className="productDetails"  ref={containerRef}>
-        <ProductDetails />
+        <ProductDetails setIsFound={setIsFound} />
         <Rate />
         <ProductSuggest />
       </div>
+      : 
+      <Box 
+      sx={{width: '100%', margin: '0 auto', textAlign: 'center'}}
+      > 
+        <img
+        src="https://bird-trading-platform.s3.ap-southeast-1.amazonaws.com/assetImage/asset/No_Product_Found.png"
+        alt="item not found"
+        style={{width: '30vw'}}
+      />
+      </Box>
+    }
     </>
   );
 }
